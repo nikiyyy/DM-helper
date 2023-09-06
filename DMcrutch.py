edb6ffa0a59195d8becc96b797b5722df3848427
unknown <nivanov997@yahoo.com>
2023-09-06 17:25:24 +0300

    added pages

--- DMcrutch.py
+++ DMcrutch.py
@@ -3,8 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return "<p>Hello, World!</p>"
 
+@app.route("/stats")
+def stats():
+    return "<p>Hello, stats!</p>"
+
+@app.route("/notes")
+def notes():
+    return "<p>Hello, notes!</p>"
+
+@app.route("/combat")
+def combat():
+    return "<p>Hello, combat!</p>"
+
+@app.route("/dictionary")
+def dictionary():
+    return "<p>Hello, dictionary!</p>"
+
+@app.route("/about")
+def about():
+    return "<p>Hello, about!</p>"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
